77714d53634f14217244fd7497264fb162f6600e
27.Matul.A <overwatchmatul@gmail.com>
2026-01-21 13:01:42 +0300

    добавилась страница с таблицами, где они будут подружены и на этой странице ссылка на пдфник который тоже позже будет дозаполнен

--- scripts/front/front_app.py
+++ scripts/front/front_app.py
@@ -5,6 +5,13 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+@app.route('/tables')
+def tables():
+    return render_template('tables.html')
+
+@app.route('/pdf')
+def pdf_view():
+    return render_template('pdf.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
