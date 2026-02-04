507fd5cef096783cefd1ef12db4f4e83184d2b4e
27.Matul.A <overwatchmatul@gmail.com>
2026-02-04 10:09:45 +0300

    в код добавлены комментарии для более удобного чтения

--- scripts/front/front_app.py
+++ scripts/front/front_app.py
@@ -1,14 +1,21 @@
 from flask import Flask, render_template
-
+#создаём веб приложение
 app = Flask(__name__)
 
+
+#Главная страница. Здесь происходит выбор дня
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+#страница таблиц для конкретного дня с номером day
 @app.route('/tables/<int:day>')
 def tables(day):
     return render_template('tables.html', day=day)
 
+
+#страница с пдфниками каждого дня
 @app.route('/pdf/<int:day>')
 def pdf_view(day):
     return render_template('pdf.html', day=day)
