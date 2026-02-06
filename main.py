7a19e7e7463109dd780f06325d0cb48b32756b5e
n3shura <162894580+n3shura@users.noreply.github.com>
2026-02-06 13:20:52 +0300

    Описание

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from table_tools.check_tables import TableManager
 
-tm = TableManager()
-for day in range(1, 5):
-    print(f"\n=== День {day} ===")
-    tm.load_day(day)
-    cutoff = tm.calculate_cutoff()
-    stats = tm.get_statistics()
-    print("Проходной балл:", cutoff)
-    print("Статистика:", stats)
+
+def print_hi(name):
+    print(f'Hi, {name}')
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+print('MEGAKNIGHT! hihiha')
